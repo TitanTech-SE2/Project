b4ed24237731ca1847cb6be0ed5f174d9e0e6c80
Mellow is fine <114035155+MellowMarcus@users.noreply.github.com>
2022-12-01 18:54:17 -0400

    Update user.py

--- App/views/user.py
+++ App/views/user.py
@@ -35,19 +35,6 @@ def userSignUP():
     return showLogin()
 
 
-@user_views.route('/api/users/upload')
-@login_required
-def userUpload():
-    return render_template('upload.html')
-
-@user_views.route('/api/users/uploader', methods=['GET', 'POST'])
-@login_required
-def userUpload();
-    if request.method = 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'File uploaded successfully'
-
 @user_views.route('/home',methods=['GET'])
 @login_required
 def get_homePage():
@@ -123,3 +110,16 @@ def makeUser():
         return jsonify({"message":"Sorry! Username exists in database already"}) 
     user = create_user(data['username'], data['password'])
     return jsonify({"message":"User Created"}) 
+
+@user_views.route('/api/users/upload')
+@login_required
+def userUpload():
+    return render_template('upload.html')
+
+@user_views.route('/api/users/uploader', methods=['GET', 'POST'])
+@login_required
+def userUpload();
+    if request.method = 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'File uploaded successfully'
