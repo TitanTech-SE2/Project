6a5fe8504ecee634ef904b5a7d7f49fdcca1d34b
Mellow is fine <114035155+MellowMarcus@users.noreply.github.com>
2022-12-03 15:39:25 -0400

    Create webforms.py

---
+++ App/webforms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
+from flask_wtf.file import FileField, FileRequired
+
+class SearchForm(FlaskForm):
+	searched = StringField("Searched", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired()])
+	password = PasswordField("Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
+class UserForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired()])
+	password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match!')])
+	password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
+	profile_pic = FileField("Profile Pic", validators = [URL()])
+	submit = SubmitField("Submit")
+
+class PasswordForm(FlaskForm):
+	password_hash = PasswordField("What's Your Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+class UploadForm(FlaskForm):
+	photo = FileField(validators=[FileAllowed(photos,'Image Files (.jpg, gif, png) Allowed'), FileRequired("File field should not be empty')
+	submit = SubmitField("Upload")					
