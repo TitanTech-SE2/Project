735f3b4e4571f54b043a728cd8108c22ebf72a6d
Mellow is fine <114035155+MellowMarcus@users.noreply.github.com>
2022-12-02 20:50:23 -0400

    Update image.py

--- App/models/image.py
+++ App/models/image.py
@@ -5,7 +5,7 @@ class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userId =  db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     rankings = db.relationship('Ranking', backref='ranking', lazy=True, cascade="all, delete-orphan")
-    source = db.Column(db.String, nullable=False, unique=True)
+    source = db.Column(db.String, nullable=True, unique=True)
 
     def __init__(self, userId):
         self.userId = userId
