2c162bb9c591c6fe3f947186aedec66d3b462582
Danz7775 <danielsuite7@gmail.com>
2022-12-07 06:02:57 +0000

    added rank to ui and small ui changes

--- App/views/ranking.py
+++ App/views/ranking.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, jsonify, request, send_from_directory
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash
 from flask_jwt import jwt_required
-
+from flask_login import login_required, current_user
 
 from App.controllers import (
     create_ranking, 
@@ -33,6 +33,41 @@ def create_ranking_action():
 
         return jsonify({"message":"User cannot rank self"})
     return jsonify({"message":"User not found"}) 
+
+@ranking_views.route('/ranking/<int:imageID>', methods=['POST'])
+@login_required
+def set_ranking_action(imageID):
+
+    data = request.form
+
+    existingRank = get_ranking_by_actors(current_user.id, imageID)
+
+    if not existingRank:
+        ranking = create_ranking(current_user.id, imageID, data['score'])
+    else: 
+        update_ranking(existingRank.id, data['score'])
+
+    flash(f"Saved rating of  {data['score']}  on  {imageID} ")
+    return render_template('homePage.html')
+
+
+    # request.get_json(force=True)
+    # data = request.form
+    # image = request.form.image
+    # update_ranking(data.id, data.score)
+    # if get_user(current_user.id) and get_image(data.id):
+    #     image = get_image(data.id)
+    #     if current_user.id != image.user:
+
+    #         prev = get_ranking_by_actors(current_user.id, data.id)
+    #         if prev:
+    #             return render_template(images.html)
+                
+    # ranking = create_ranking(current_user.id, data['id'], data['score'])
+        #     return render_template(images.html)
+
+        # return render_template(images.html)
+    # return render_template(images.html)
 
 @ranking_views.route('/api/rankings', methods=['GET'])
 def get_all_rankings_action():
